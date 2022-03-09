92024dabc1d38d46d67d5d03930a09ef37d17c1a
Alisyad9 <76549147+Alisyad9@users.noreply.github.com>
2022-03-09 22:16:29 +0000

    home landing page done

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,38 @@
 import React from 'react';
 import './Home.css';
+import logo from '../img/logo.png';
 function Home() {
   return (
     <div className="home">
-      {' '}
-      <h1>home</h1>
+      <div className="home__bg">
+        <div className=" header d__flex align__items__center px___30">
+          <div className="logo">
+            <img src={logo} />
+          </div>
+          <nav className="navigation pxy__30">
+            <ul className="navbar d__flex">
+              <a href="#Home">
+                <li className="nav__items mx__15">Home</li>
+              </a>
+              <a href="#About">
+                <li className="nav__items mx__15">About</li>
+              </a>
+              <a href="#Services">
+                <li className="nav__items mx__15">Services</li>
+              </a>
+              <a href="#Portfolio">
+                <li className="nav__items mx__15">Portfolio</li>
+              </a>
+              <a href="#Blog">
+                <li className="nav__items mx__15">Blog</li>
+              </a>
+              <a href="#Contact">
+                <li className="nav__items mx__15">Contact</li>
+              </a>
+            </ul>
+          </nav>
+        </div>
+      </div>
     </div>
   );
 }
